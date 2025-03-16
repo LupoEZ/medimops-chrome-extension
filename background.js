7ceb309379334d0a9d12416f74ac69390770d826
ez12@kyso <eric.zeneljaj@gmail.com>
2025-03-16 18:42:53 +0100

    Added new files, updated manifest and added TODO's in the background.js

---
+++ background.js
@@ -0,0 +1,18 @@
+//TODO: Store and retrieve wishlist items using chrome.storage
+    //TODO: Try out fetch method with medimops.de/MeinMerkzettel request and analyze response structure
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO: Use chrome.alarms to check prices at intervals
+//TODO: Send a chrome.notifications alert if a discount is found
+
